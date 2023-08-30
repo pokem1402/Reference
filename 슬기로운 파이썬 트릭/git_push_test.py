2438e86849787f3ebfbc6c97cfa6d839e666e553
Wonbin Kim <wbkim91@gmail.com>
2023-08-30 15:17:41 +0900

    list item added

--- 슬기로운 파이썬 트릭/git_push_test.py
+++ 슬기로운 파이썬 트릭/git_push_test.py
@@ -1,16 +1,18 @@
 
-리스트 = ["Alice", 'Bob', 'Dilbert']
+리스트 = ["Alice", 'Bob', 'Dilbert','Charlie']
 
 리스트_개행 = [
     "Alice",
     'Bob',
-    'Dilbert'
+    'Dilbert',
+    'Charlie'
 ]
 
 리스트_개행_마지막_요소_쉼표 = [
     "Alice",
     'Bob',
     'Dilbert',
+    'Charlie',
 ]
 
 
